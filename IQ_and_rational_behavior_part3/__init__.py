232182038bd3bd4055e8003ccb418004b53a3f95
mathe <mathexplorer314@gmail.com>
2023-04-14 20:58:10 -0500

    Almost done with part 3

--- IQ_and_rational_behavior_part3/__init__.py
+++ IQ_and_rational_behavior_part3/__init__.py
@@ -8,7 +8,7 @@ Your app description
 
 class C(BaseConstants):
     NAME_IN_URL = 'IQ_and_rational_behavior_part3'
-    PLAYERS_PER_GROUP = None
+    PLAYERS_PER_GROUP = 2
     NUM_ROUNDS = 1
 
 
@@ -23,6 +23,7 @@ class Group(BaseGroup):
 class Player(BasePlayer):
     lottery_choice = models.IntegerField(widget=widgets.RadioSelect, choices=[1, 2, 3, 4, 5, 6])
     dictator_choice = models.IntegerField()
+    BI_choice = models.IntegerField(widget=widgets.RadioSelect, choices=[1, 2, 3, 4, 5, 6])
 
 # PAGES
 class Introduction(Page):
@@ -33,14 +34,26 @@ class Lottery(Page):
     form_model = 'player'
     form_fields = ['lottery_choice']
 
+    def before_next_page(player: Player, timeout_happened):
+        player.participant.lottery_choice=player.lottery_choice
+
 # PAGES
 class Dictator(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['dictator_choice']
+
+    def before_next_page(player: Player, timeout_happened):
+        player.participant.dictator_choice=player.dictator_choice
+
+        #TODO: make 2 player
 
 # PAGES
 class BackwardsInduction(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['BI_choice']
 
+    def before_next_page(player: Player, timeout_happened):
+        player.participant.BI_choice=player.BI_choice
 
 class ResultsWaitPage(WaitPage):
     pass
