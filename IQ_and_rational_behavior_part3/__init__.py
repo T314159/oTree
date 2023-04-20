ba07e6f4b7041379619825c283523cf4333c0bf6
mathe <mathexplorer314@gmail.com>
2023-04-20 13:33:34 -0500

    Started on Results and payment calculation

--- IQ_and_rational_behavior_part3/__init__.py
+++ IQ_and_rational_behavior_part3/__init__.py
@@ -25,9 +25,11 @@ class Player(BasePlayer):
     dictator_choice = models.IntegerField()
     BI_choice = models.IntegerField(widget=widgets.RadioSelect, choices=[1, 2, 3, 4, 5, 6])
 
+
 # PAGES
 class Introduction(Page):
     pass
+
 
 # PAGES
 class Lottery(Page):
@@ -36,6 +38,7 @@ class Lottery(Page):
 
     def before_next_page(player: Player, timeout_happened):
         player.participant.lottery_choice=player.lottery_choice
+
 
 # PAGES
 class Dictator(Page):
@@ -47,6 +50,7 @@ class Dictator(Page):
         for opponent in player.get_others_in_group():
             opponent.participant.dictator_from_others = 100-player.dictator_choice
 
+
 # PAGES
 class BackwardsInduction(Page):
     form_model = 'player'
@@ -55,12 +59,43 @@ class BackwardsInduction(Page):
     def before_next_page(player: Player, timeout_happened):
         player.participant.BI_choice=player.BI_choice
 
+
 class ResultsWaitPage(WaitPage):
     pass
 
 
 class Results(Page):
-    pass
+    def vars_for_template(player: Player):
+        import random
+        part1_earned = 0
+        part2_earned = 0
+        part3_earned = 0
+
+        selected = random.choices([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], k=2)
+        for question_index in selected:
+            if player.participant.ravens_results[question_index] == 1:
+                part1_earned += 100
+
+        part2_selected = random.randint(1,3)
+        # Get info from past.
+
+        part3_selected = random.choice(["Lottery", "Allocation", "Reasoning"])
+        if selected == "Lottery":
+            lottery_result = random.randint(1,2)
+            payoff_matrix = [[140,140],[120,180],[100,220],[80,260],[60,300],[10, 350]]
+            part3_earned += payoff_matrix[player.lottery_choice-1][lottery_result-1]
+        elif selected == "Allocation":
+            pass
+            #get info on dictator
+        else:
+            if player.BI_choice == 1:
+                part3_earned = 100
+
+        return dict(
+            part1_text = part1_text,
+            part2_text=part2_text,
+            part3_text = part3_text
+        )
 
 
 page_sequence = [Introduction, Lottery, Dictator, BackwardsInduction, ResultsWaitPage, Results]
